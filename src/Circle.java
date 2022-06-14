ed3352f154edd7bd31f6d75545ef7875882a264f
Barash Anton <BarashASUA@gmail.com>
2022-06-14 15:30:40 +0300

    радіус кола

--- src/Circle.java
+++ src/Circle.java
@@ -1,2 +1,12 @@
+import java.util.Scanner;
+
 public class Circle {
+    public static void main(String[] args) {
+        System.out.println("Введіть радіус кола для розрахунку його площі: ");
+        Scanner circleRadius = new Scanner(System.in);
+        final double PI = 3.14;    // константа Р = зі значенням 3.14
+        double r = circleRadius.nextDouble();   // радіус кола вноситься самостійно
+        double S = PI*(r*r);    // формула площі кола - πR2
+        System.out.println("Площа круга виміряна за формулою πR2 з числа " + r + " складає " + S);    // вивод в консоль
+    }
 }
--- src/Volume.java
+++ src/Volume.java
@@ -1,3 +1,27 @@
-public class Volume {
+import java.util.Scanner;
 
+public class Volume {
+    public static void main(String[] args) {
+        System.out.println("Введіть радіус: ");
+        Scanner radius = new Scanner(System.in);    // радиус
+        double R = radius.nextDouble();
+        System.out.println("Введіть висоту: ");
+        Scanner haight = new Scanner(System.in);
+        final double PI = 3.14;
+        double h = haight.nextDouble();                        // висота циліндра
+        double V = PI* ((R * R) * h);                      // S площа
+
+        System.out.println("Об'єм циліндру за формулою V = πR2 h складає " + V);
+        System.out.println();
+        double S = 2 * ((PI * R) *(h + R));
+        System.out.println("Площа всієї поверхні циліндру за формулою S = 2πR(h + r) складає " + S);
+    }
 }
+
+
+
+//    Используя IntelliJ IDEA, создайте класс Volume. Напишите программу расчета объема
+//        - V и площади поверхности -S цилиндра. Объем V цилиндра радиусом – R и высотой – h, вычисляется по формуле:
+//        V = πR2 h
+//        Площадь S поверхности цилиндра вычисляется по формуле: S = 2πR2 + 2πR2 = 2πR(R+h) Результаты расчетов выведите на экран.
+
